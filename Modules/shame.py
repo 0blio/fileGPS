aaa7b5812bfc9e7f76685262b7b2fe17aae41e3d
Michele Cisternino <miki.cisternino@gmail.com>
2019-06-17 15:40:09 +0200

    Update shame.py

--- Modules/shame.py
+++ Modules/shame.py
@@ -4,17 +4,27 @@
 """
     Module name: shame
     Coded by: Michele '0blio' Cisternino
-    Version: 0.1
+    Version: 0.2
 
     Description:
 
-        This module generate different types of hashes of the filename.
+        This modules generate different hashed version of the filenames.
+        It also append the generated values to a list of common upload directories.
 """
 
-from Helpers.utilities import *
+output = []
 
-# Taking the name and the ext of the file you uploaded
+# Taking the list of common directories
+common_dirs = open("txt/common-directories.txt", "r").read().splitlines()
+
+# Taking the name and the ext of the file the user uploaded
 fname, fext = os.path.splitext(filename)
 
-# Output the filename in clear-text and hashed in various ways
-output = [o + fext for o in [fname, string_to_md5(fname), string_to_sha1(fname), string_to_sha256(fname)]]
+# Creating a list with the clear-text filename and various filename hashes
+hashed_filenames = [o + fext for o in [fname, string_to_md5(fname), string_to_sha1(fname), string_to_sha256(fname)]]
+
+# Output hashed_filenames
+output += hashed_filenames
+
+# Output common directories + hashed_filenames
+output += [dir + f for dir in common_dirs for f in hashed_filenames]
